4c9a56d08336a33414cea8d61bdf0bf97d62e9ef
Adrian Duarte <aadm12692@gmail.com>
2019-02-24 19:48:01 -0300

    Adds Tiny DB util

---
+++ app/src/main/java/com/example/paymentapp/utils/TinyDB.java
@@ -0,0 +1,256 @@
+package com.example.paymentapp.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+import com.example.paymentapp.models.data.Payment;
+import com.google.gson.Gson;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+public class TinyDB {
+
+    // Constants
+    private static final String PAYMENT = "payment";
+
+    // Attributes
+    private SharedPreferences preferences;
+
+    // Constructors
+    public TinyDB(Context appContext) {
+        preferences = PreferenceManager.getDefaultSharedPreferences(appContext);
+    }
+
+    // Getters
+
+    public int getInt(String key) {
+        return preferences.getInt(key, 0);
+    }
+
+    public ArrayList<Integer> getListInt(String key) {
+        String[] myList = TextUtils.split(preferences.getString(key, ""), "‚‗‚");
+        ArrayList<String> arrayToList = new ArrayList<>(Arrays.asList(myList));
+        ArrayList<Integer> newList = new ArrayList<>();
+
+        for (String item : arrayToList)
+            newList.add(Integer.parseInt(item));
+
+        return newList;
+    }
+
+    public long getLong(String key, long defaultValue) {
+        return preferences.getLong(key, defaultValue);
+    }
+
+    public float getFloat(String key) {
+        return preferences.getFloat(key, 0);
+    }
+
+    public double getDouble(String key, double defaultValue) {
+        String number = getString(key);
+
+        try {
+            return Double.parseDouble(number);
+
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
+    public ArrayList<Double> getListDouble(String key) {
+        String[] myList = TextUtils.split(preferences.getString(key, ""), "‚‗‚");
+        ArrayList<String> arrayToList = new ArrayList<>(Arrays.asList(myList));
+        ArrayList<Double> newList = new ArrayList<>();
+
+        for (String item : arrayToList)
+            newList.add(Double.parseDouble(item));
+
+        return newList;
+    }
+
+    public ArrayList<Long> getListLong(String key) {
+        String[] myList = TextUtils.split(preferences.getString(key, ""), "‚‗‚");
+        ArrayList<String> arrayToList = new ArrayList<>(Arrays.asList(myList));
+        ArrayList<Long> newList = new ArrayList<>();
+
+        for (String item : arrayToList)
+            newList.add(Long.parseLong(item));
+
+        return newList;
+    }
+
+    public String getString(String key) {
+        return preferences.getString(key, "");
+    }
+
+    public ArrayList<String> getListString(String key) {
+        return new ArrayList<>(Arrays.asList(TextUtils.split(preferences.getString(key, ""), "‚‗‚")));
+    }
+
+    public boolean getBoolean(String key) {
+        return preferences.getBoolean(key, false);
+    }
+
+    public ArrayList<Boolean> getListBoolean(String key) {
+        ArrayList<String> myList = getListString(key);
+        ArrayList<Boolean> newList = new ArrayList<>();
+
+        for (String item : myList) {
+            if (item.equals("true")) {
+                newList.add(true);
+            } else {
+                newList.add(false);
+            }
+        }
+
+        return newList;
+    }
+
+    public ArrayList<Object> getListObject(String key, Class<?> mClass){
+        Gson gson = new Gson();
+        ArrayList<String> objStrings = getListString(key);
+        ArrayList<Object> objects =  new ArrayList<>();
+        for(String jObjString : objStrings){
+            Object value  = gson.fromJson(jObjString,  mClass);
+            objects.add(value);
+        }
+        return objects;
+    }
+
+    public <T> T getObject(String key, Class<T> classOfT){
+        String json = getString(key);
+        Object value = new Gson().fromJson(json, classOfT);
+        if (value == null)
+            throw new NullPointerException();
+        return (T)value;
+    }
+
+    public Payment getPayment() {
+        try {
+            return getObject(PAYMENT, Payment.class);
+        } catch (Exception e) {
+            return new Payment();
+        }
+    }
+
+    // Put methods
+
+    public void putInt(String key, int value) {
+        checkForNullKey(key);
+        preferences.edit().putInt(key, value).apply();
+    }
+
+    public void putListInt(String key, ArrayList<Integer> intList) {
+        checkForNullKey(key);
+        Integer[] myIntList = intList.toArray(new Integer[intList.size()]);
+        preferences.edit().putString(key, TextUtils.join("‚‗‚", myIntList)).apply();
+    }
+
+    public void putLong(String key, long value) {
+        checkForNullKey(key);
+        preferences.edit().putLong(key, value).apply();
+    }
+
+    public void putListLong(String key, ArrayList<Long> longList) {
+        checkForNullKey(key);
+        Long[] myLongList = longList.toArray(new Long[longList.size()]);
+        preferences.edit().putString(key, TextUtils.join("‚‗‚", myLongList)).apply();
+    }
+
+    public void putFloat(String key, float value) {
+        checkForNullKey(key);
+        preferences.edit().putFloat(key, value).apply();
+    }
+
+    public void putDouble(String key, double value) {
+        checkForNullKey(key);
+        putString(key, String.valueOf(value));
+    }
+
+    public void putListDouble(String key, ArrayList<Double> doubleList) {
+        checkForNullKey(key);
+        Double[] myDoubleList = doubleList.toArray(new Double[doubleList.size()]);
+        preferences.edit().putString(key, TextUtils.join("‚‗‚", myDoubleList)).apply();
+    }
+
+    public void putString(String key, String value) {
+        checkForNullKey(key); checkForNullValue(value);
+        preferences.edit().putString(key, value).apply();
+    }
+
+    public void putListString(String key, ArrayList<String> stringList) {
+        checkForNullKey(key);
+        String[] myStringList = stringList.toArray(new String[stringList.size()]);
+        preferences.edit().putString(key, TextUtils.join("‚‗‚", myStringList)).apply();
+    }
+
+    public void putBoolean(String key, boolean value) {
+        checkForNullKey(key);
+        preferences.edit().putBoolean(key, value).apply();
+    }
+
+    public void putListBoolean(String key, ArrayList<Boolean> boolList) {
+        checkForNullKey(key);
+        ArrayList<String> newList = new ArrayList<>();
+
+        for (Boolean item : boolList) {
+            if (item) {
+                newList.add("true");
+            } else {
+                newList.add("false");
+            }
+        }
+
+        putListString(key, newList);
+    }
+
+    public void putObject(String key, Object obj){
+        checkForNullKey(key);
+        Gson gson = new Gson();
+        putString(key, gson.toJson(obj));
+    }
+
+    public void putPayment(Payment payment){
+        Gson gson = new Gson();
+        putString(PAYMENT, gson.toJson(payment));
+    }
+
+    public void putListObject(String key, ArrayList<Object> objArray){
+        checkForNullKey(key);
+        Gson gson = new Gson();
+        ArrayList<String> objStrings = new ArrayList<>();
+        for(Object obj : objArray){
+        	objStrings.add(gson.toJson(obj));
+        }
+        putListString(key, objStrings);
+    }
+
+    public void remove(String key) {
+        preferences.edit().remove(key).apply();
+    }
+
+    public void clear() {
+        preferences.edit().clear().apply();
+    }
+
+    public Map<String, ?> getAll() {
+        return preferences.getAll();
+    }
+
+    public void checkForNullKey(String key){
+        if (key == null){
+            throw new NullPointerException();
+        }
+    }
+
+    public void checkForNullValue(String value){
+        if (value == null){
+            throw new NullPointerException();
+        }
+    }
+
+}
